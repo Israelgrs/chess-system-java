1edf16a0be1e627950021d523df9e6befd12a9f9
Israel Gouveia <israel_grs@live.com>
2024-06-15 21:52:09 -0300

    Piece move count

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -6,6 +6,7 @@ import boardgame.Position;
 
 public abstract class ChessPiece extends Piece{
 	private Color color;
+	private int moveCount;
 
 	public ChessPiece(Board board, Color color) {
 		super(board);
@@ -27,5 +28,17 @@ public abstract class ChessPiece extends Piece{
 	
 	public ChessPosition getChessPiece(){
 		return ChessPosition.fromPosition(position);
+	}
+	
+	public void increaseMoveCount() {
+		moveCount++;
+	} 
+	
+	public void decreaseMoveCount() {
+		moveCount--;
+	}
+	
+	public int getMoveCount() {
+		return moveCount;
 	}
 }
